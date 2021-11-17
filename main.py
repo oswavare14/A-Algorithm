1a97c653a9bf899a9f56abcacac60f2dfb999d41
Oswaldo <oswavare14@hotmail.com>
2021-11-16 18:16:11 -0600

    a star

--- main.py
+++ main.py
@@ -1,6 +1,73 @@
+class Nodo():
+
+    def __init__(self, padre=None, pos=None):
+        self.padre = padre
+        self.pos = pos
+
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.pos == other.pos
+
+
+def astar(mapa, start, end):
+    raiz = Nodo(None, start)
+    raiz.g = raiz.h = raiz.f = 0
+    final = Nodo(None, end)
+    final.g = final.h = final.f = 0
+    lista_A = []
+    lista_B = []
+    lista_A.append(raiz)
+
+    while len(lista_A) > 0:
+        nodo_actual = lista_A[0]
+        index_actual = 0
+        for index, item in enumerate(lista_A):
+            if item.f < nodo_actual.f:
+                nodo_actual = item
+                index_actual = index
+
+        lista_A.pop(index_actual)
+        lista_B.append(nodo_actual)
+
+        # Encontrando el Final
+        if nodo_actual == final:
+            camino = []
+            c = nodo_actual
+            while c is not None:
+                camino.append(c.pos)
+                c = c.padre
+            return camino[::-1] # Return reversed path
+
+        hijo = []
+        for k in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+            pos_nodo = (nodo_actual.pos[0] + k[0], nodo_actual.pos[1] + k[1])
+            # Posicion sea dentro del mapa, una posicion valida
+            if pos_nodo[0] > (len(mapa) - 1) or pos_nodo[0] < 0 or pos_nodo[1] > (len(mapa[len(mapa)-1]) -1) or pos_nodo[1] < 0:
+                continue
+            # Pueda caminar en el mapa
+            if mapa[pos_nodo[0]][pos_nodo[1]] != '-':
+                continue
+            nuevo = Nodo(nodo_actual, pos_nodo)
+            hijo.append(nuevo)
+
+        for i in hijo:
+            for closed_child in lista_B:
+                if i == closed_child:
+                    continue
+            # Calculo del heuristico
+            i.g = nodo_actual.g + 1
+            i.h = ((i.pos[0] - final.pos[0]) ** 2) + ((i.pos[1] - final.pos[1]) ** 2)
+            i.f = i.g + i.h
+            for j in lista_A:
+                if i == j and i.g > j.g:
+                    continue
+            lista_A.append(i)
 
 def main():
-    txt = open('mapa_3.txt', 'r')
+    txt = open('mapa_2.txt', 'r')
     tamaño = txt.readline()
     columnas = ""
     filas = ""
@@ -22,7 +89,7 @@ def main():
     mapa = []
     positionD = []
     positionO = []
-    start = []
+    inicio = []
 
     for i in range(0,filas):
         temp = []
@@ -45,10 +112,15 @@ def main():
                     tempS = []
                     tempS.append(i)
                     tempS.append(contTemp)
-                    start.append(tempS)
+                    inicio.append(tempS)
                 temp.append(j)
                 contTemp+=1
         mapa.append(temp)
+
+    t = (0,0)
+    y = (3,4)
+    path = astar(mapa, t, y)
+    print(path)
 
     if contD != contO:
         print("Error. Las cargas o las bodegas")
